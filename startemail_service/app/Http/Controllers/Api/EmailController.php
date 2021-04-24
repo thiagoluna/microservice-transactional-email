da0c9b20a7b1b02e167bce86b2074b21fbc3cba9
Thiago Luna <thiagoluna2@gmail.com>
2021-04-24 18:14:10 -0300

    Model Controller Route Email

---
+++ startemail_service/app/Http/Controllers/Api/EmailController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Email;
+use Illuminate\Http\Request;
+
+class EmailController extends Controller
+{
+    public function sendEmail(Request $request)
+    {
+        $email = $request->all();
+        Email::create($email);
+
+        return response()->json(['success' => 'Email saved'], 201);
+    }
+
+    public function listEmail()
+    {
+        $emails = Email::All();
+
+        return response()->json($emails);
+    }
+}
